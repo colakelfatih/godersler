1cad5c333269756f881f6d5f2c7ba592f5847c80
colakelfatih <info@fatihcolakel.com.tr>
2021-05-17 22:37:53 +0300

    constants

---
+++ 09_constant/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*5-3.14-passed-true*/
+	/* r := 3.0
+
+	const pi float64 = 3.14 //sabit değer değişmez
+
+	areaCircle := pi * (math.Pow(r, 2))
+	fmt.Println(areaCircle) */
+
+	/* 	const x int = 2
+	   	const y float64 = 3.4
+	   	const z string = "test"
+	   	const t bool = false
+
+	   	fmt.Printf("%T, %v\n", x, x)
+	   	fmt.Printf("%T, %v\n", y, y)
+	   	fmt.Printf("%T, %v\n", z, z)
+	   	fmt.Printf("%T, %v\n", t, t) */
+
+	const x = 2
+
+	fmt.Printf("%T, %v\n", x, x)
+
+}
