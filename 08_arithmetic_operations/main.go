722a1e627a02b0f6505dbe415b6ae0bb05e0bb87
colakelfatih <info@fatihcolakel.com.tr>
2021-05-17 22:20:37 +0300

    arithmetic

---
+++ 08_arithmetic_operations/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//x, y := 15, 10
+	//fmt.Printf("%T, %v\n", x, x)
+	//fmt.Printf("%T, %v\n", y, y)
+	//fmt.Printf("%T, %v\n", (x + y), (x + y))
+	//fmt.Printf("%T, %v\n", (x - y), (x - y))
+	//fmt.Printf("%T, %v\n", (x * y), (x * y))
+	//fmt.Printf("%T, %v\n", (x / y), (x / y))
+
+	x := 10
+	fmt.Println(x)
+
+	x = x + 1
+	fmt.Println(x)
+
+	x++
+	fmt.Println(x)
+
+}
