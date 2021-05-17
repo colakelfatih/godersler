14a9c5ea9da836df81257b03cc49ac61b20fea72
colakelfatih <info@fatihcolakel.com.tr>
2021-05-17 20:18:48 +0300

    terminoloji

---
+++ 04_practice/main.go
@@ -0,0 +1,55 @@
+/* // Package clause
+package main
+
+// Import statement
+import "fmt"
+
+// My code
+func main() {
+
+	 	var studentName string = "John Doe"
+	   	var grade int = 77
+	   	var isPassed bool = true */
+/*
+	 	var studentName = "John Doe"
+	   	var grade = 77
+	   	var isPassed = true
+
+	studentName := "John Doe"
+	grade := 70
+	isPassed := true
+
+	fmt.Println(studentName)
+	fmt.Println(grade)
+	fmt.Println(isPassed)
+
+} */
+
+// Package clause
+package main
+
+import "fmt"
+
+// Import statement
+
+// My code
+func main() {
+
+	//declaration
+	/* var studentName string
+	fmt.Println(studentName) */
+
+	//Assign
+	/* var studentName string = "John Doe"
+	fmt.Println(studentName) */
+
+	//Initialization
+	/* var studentName string = "John Doe" //initialize a value
+	fmt.Println(studentName) */
+
+	// := vs = differents
+	studentName := "Ahmet Selman"
+	studentName = "Fatih Çolakel"
+	fmt.Println(studentName)
+
+}
