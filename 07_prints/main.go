2b875134b88e0329c22f1c68e4b86e795f4ec840
colakelfatih <info@fatihcolakel.com.tr>
2021-05-17 21:46:03 +0300

    print

---
+++ 07_prints/main.go
@@ -0,0 +1,31 @@
+package main
+
+//var x = 10
+
+func main() {
+
+	// print - printf - println
+	/*
+		fmt.Println("Merhaba")
+		fmt.Print("Merhaba")
+		fmt.Printf("Merhaba") */
+
+	//name := "Fatih"
+
+	/* fmt.Println(name)
+	fmt.Print(name)
+	fmt.Printf(name) */
+	//fmt.Print("Benim adım", name)
+	//fmt.Printf("Benim adım %v %T", name, name)
+	//fmt.Printf("Benim adım %v %T", name, name)
+
+	//VISIBILITY
+
+	//x := 10
+
+	//fmt.Println(x)
+	//Go camel case using
+	var coin string
+	var coinType string
+
+}
