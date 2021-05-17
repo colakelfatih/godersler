27468cad42315c0a39cf0627fc2e721351020f98
colakelfatih <info@fatihcolakel.com.tr>
2021-05-17 18:28:15 +0300

    variable types

--- 01_variable_declaration/main.go
+++ 01_variable_declaration/main.go
@@ -1,11 +1,30 @@
+// Package clause
 package main
 
+// Import statement
 import (
 	"fmt"
 )
 
+// My code
 func main() {
+	//var name string // var - name of variable - static type
+	//name = "Fatih"
+
 	var name string = "Fatih"
-	fmt.Println("Tamamlandı!", name)
+
+	var first, second string = "Ahmet", "Mehmet"
+
+	degisken := "Test"
+
+	var age int
+	age = 40
+
+	var isMarried bool
+	isMarried = true
+
+	//var isMarried bool = true
+
+	fmt.Println(name, age, isMarried, first, second, degisken)
 
 }
